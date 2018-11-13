e9c63da75f54ac7e807d9e659b7238193447423e
chikako-LLB <chikakolilleboe@gmail.com>
2018-11-14 00:14:07 +0900

    Revert "Add group model and contoroller"
    This reverts commit ce5d4ff449e66fe92d75c672d4f04d79794b932e.

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,12 +1,6 @@
 class MessagesController < ApplicationController
 
-  def show
-    user = User.find(params[:id])
-    @nickname = user.nickname
-    @tweets = user.tweets.page(params[:page]).per(5).order("created_at DESC")
-  end
-
-  def index
-  end
+   def index
+   end
 end
 
