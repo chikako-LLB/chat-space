a8d5b451f5e66bad6b652fd1b1d5f2b2451230da
chikako-LLB <chikakolilleboe@gmail.com>
2018-11-18 20:03:27 +0900

    Revised %Y年
    Revised %Y年
    Beause it was written 2times.

--- app/helpers/message_helper.rb
+++ app/helpers/message_helper.rb
@@ -1,6 +1,6 @@
 module MessageHelper
 
   def format_posted_time(posting_time)
-    posting_time.in_time_zone("Tokyo").strftime("%Y年%Y年%m月%d日 %H:%M:%S")
+    posting_time.in_time_zone("Tokyo").strftime("%Y年%m月%d日 %H:%M:%S")
   end
 end
